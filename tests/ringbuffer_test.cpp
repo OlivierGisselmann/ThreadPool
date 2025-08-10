38f918adee4341a9dc7b1258ce9e608ca384f4ab
OlivierGisselmann <gisselmann.olivier@gmail.com>
2025-08-10 07:55:28 +0200

    test: add all ringbuffer tests

---
+++ tests/ringbuffer_test.cpp
@@ -0,0 +1,63 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <thread_pool/ring_buffer.hpp>
+
+TEST_CASE( "Pop empty ring buffer returns false", "[ring_buffer]")
+{
+    ThreadPool::RingBuffer<int, 5> queue;
+
+    int result = 0;
+
+    REQUIRE(queue.Pop(result) == false);
+}
+
+TEST_CASE( "Push full ring buffer returns false", "[ring_buffer]")
+{
+    ThreadPool::RingBuffer<int, 1> queue;
+
+    queue.Push(42);
+
+    REQUIRE(queue.Push(21) == false);
+}
+
+TEST_CASE( "Ring buffer pushes and pops", "[ring_buffer]" )
+{
+    // Create a ring queue with 50 int slots
+    ThreadPool::RingBuffer<int, 50> queue;
+
+    int result = 0;
+
+    // Can fill the queue
+    for(int i = 0; i < 49; ++i)
+    {
+        REQUIRE(queue.Push(i) == true);
+    }
+
+    // Can pop queue data
+    for(int i = 0; i < 49; ++i)
+    {
+        queue.Pop(result);
+        REQUIRE( result == i );
+    }
+}
+
+TEST_CASE( "Ring buffer has correct values", "[ring_buffer]")
+{
+    ThreadPool::RingBuffer<int, 4> queue;
+
+    int result = 0;
+
+    // FIFO
+    queue.Push(21);
+    queue.Push(42);
+    queue.Push(96);
+
+    queue.Pop(result);
+    REQUIRE(result == 21);
+
+    queue.Pop(result);
+    REQUIRE(result == 42);
+
+    queue.Pop(result);
+    REQUIRE(result == 96);
+}
